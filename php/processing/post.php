ae4d413435fc15c40d41460c3199851cbb0ed837
Neolectron <xeolectron@gmail.com>
2014-10-08 14:00:08 +0200

    suppression des debug informations

--- php/processing/post.php
+++ php/processing/post.php
@@ -1,19 +1,9 @@
 <?php
-echo '<pre>';
-
     if(isset($_POST['action']) && ($_POST['action'] == 'newarticle'))
     {
         if( isset($_POST['article']) && isset($_POST['articleauthor']) && isset($_POST['articlesince']) && isset($_POST['articletitle']) && $_POST['action'])
         {
-
-
-
-            echo $_POST['article'];      //DEBUG
-            echo $_POST['articletitle']; //DEBUG
-            echo $_POST['articleauthor'];//DEBUG
-            echo $_POST['articlesince']; //DEBUG
-
-            $article        = preg_replace('~<\s*\bscript\b[^>]*>(.*?)<\s*\/\s*script\s*>~is', '', $_POST['article'] );
+            $article        = preg_replace('~<\s*\bscript\b[^>]*>(.*?)<\s*\/\s*script\s*>~is', 'DEL', $_POST['article'] );
             $articletitle   = htmlspecialchars($_POST['articletitle'] , ENT_QUOTES);
             $articleauthor  = htmlspecialchars($_POST['articleauthor'], ENT_QUOTES);
             $articlesince   = htmlspecialchars($_POST['articlesince'] , ENT_QUOTES);
@@ -23,9 +13,12 @@ echo '<pre>';
             $result = $craftblog->query("INSERT INTO article(authorid,articletitle,articlesince,article) VALUES((SELECT userid FROM users WHERE username = '$articleauthor'),'$articletitle','$articlesince','$article') ");
             if($result)
             {
-                echo "done.";
+                //its ok
+            }
+            else
+            {
+                //its not
             }
 
         }
     }
-echo '</pre>';
